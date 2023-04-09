b79b25dddf1bb94db10f7a1333fbc1898cc23dbb
amigo <amigo4988@outlook.com>
2023-04-09 15:35:25 +0300

    feat: connect postgres db

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'courtcases',
+        'USER': 'lawyer',
+        'PASSWORD': 'testuser',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
