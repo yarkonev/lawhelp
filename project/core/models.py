387857e8c6bb3d5f6adfc65552b859b4e8270758
amigo <amigo4988@outlook.com>
2023-04-09 16:59:55 +0300

    feat: add db models

--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class LegalEntity(models.Model):
+    full_name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=30)
+    ogrn = models.CharField(max_length=13)
+    inn = models.CharField(max_length=11)
+    address = models.TextField(max_length=200)
+    
+    class Meta:
+        abstract = True
+        
+    
+class Plaintiff(LegalEntity):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+
+class Defendant(LegalEntity):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    
+class Court(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField(max_length=200)    
