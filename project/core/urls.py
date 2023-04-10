c54f53550854dee9a5f8f1e8f315a158337c4fb3
amigo <amigo4988@outlook.com>
2023-04-10 15:35:44 +0300

    add page with a list of court cases

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index
+from . import views
 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    path('cases/', views.cases, name='cases'),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Case
 
 
 def index(request):
     return render(request, 'index.html')
+
+def cases(request):
+    cases = Case.objects.all()
+    context = {
+        'cases': cases,
+    }
+    return render(request, 'cases.html', context)
