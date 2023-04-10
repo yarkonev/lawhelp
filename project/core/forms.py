1a5212d71a873e045b781fd660e4247ba647855e
Yaroslav Konev <konev.yar@outlook.com>
2023-04-10 21:08:23 +0300

    update forms added labels

--- project/core/forms.py
+++ project/core/forms.py
@@ -6,3 +6,9 @@ class CaseForm(forms.ModelForm):
     class Meta:
         model = Case
         fields = ['number', 'court', 'card', 'plaintiff', 'defendant']
+        labels = {'number': 'Номер дела',
+                  'court': 'Суд',
+                  'card': 'Карточка дела',
+                  'plaintiff': 'Истец',
+                  'defendant': 'Ответчик',
+                  }
