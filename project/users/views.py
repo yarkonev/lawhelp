042be9f6a25db3147cd48ca967fff7f3a25fdf4d
konevyar <konev.yar@outlook.com>
2023-05-04 15:39:45 +0300

    refactor: users views PEP8

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
@@ -13,12 +12,22 @@ class SignupView(CreateView):
     template_name = 'users/signup.html'
 
     def form_valid(self, form):
-        response = super().form_valid(form)
+        """
+        Validates a form and logs in the user if it exists in the database.
+
+        Args:
+        - form: A Django form instance with cleaned_data attribute
+        containing email and password1.
+
+        Returns:
+        - The result of the parent class's form_valid method.
+        """
         email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password1')
         user = authenticate(email=email, password=password)
-        login(self.request, user)
-        return response
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
 class CustomLoginView(LoginView):
