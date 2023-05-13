51a0dd3a3a640751cbb2069e822eb3a6fda386e4
konevyar <konev.yar@outlook.com>
2023-05-13 17:11:19 +0300

    front-end: change page templates Changed: index, login, register. Also modified forms to match pages style.

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,15 +1,43 @@
 from django import forms
 from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
                                        UserCreationForm)
+from django.forms.widgets import EmailInput
 from django.utils.translation import gettext_lazy as _
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """
+    Custom registration form that uses email instead of username.
+    Added widgets with page style matching.
+    """
     email = forms.EmailField(error_messages={
         'unique': 'Аккаунт с такой почтой уже зарегистрирован.'
-        })
+        },
+        widget=EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Email',
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password'}
+        )
+    )
+
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Repeat password'}
+        )
+    )
 
     class Meta:
         model = CustomUser
@@ -26,13 +54,24 @@ class CustomUserChangeForm(UserChangeForm):
 class LoginForm(AuthenticationForm):
     """
     Custom authentication form that uses email instead of username.
+    Added widgets with page style matching.
     """
     username = forms.EmailField(
         label=_("Email"),
-        widget=forms.TextInput(attrs={'autofocus': True}))
+        widget=EmailInput(attrs={
+            'autofocus': True,
+            'class': 'form-control',
+            'placeholder': 'Email'}
+            )
+    )
     password = forms.CharField(
         label="Password", strip=False,
-        widget=forms.PasswordInput)
+        widget=forms.PasswordInput(attrs={
+            'autofocus': True,
+            'class': 'form-control',
+            'placeholder': 'Password'}
+            )
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
