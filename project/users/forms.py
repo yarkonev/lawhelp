6bc1ade189ded6ab062f002b08d3e496960909bd
konevyar <konev.yar@outlook.com>
2023-05-21 20:17:30 +0300

    add placeholders to new login-signup forms

--- project/users/forms.py
+++ project/users/forms.py
@@ -7,10 +7,15 @@ User = get_user_model()
 
 
 class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.EmailField(widget=forms.EmailInput)
+    username = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
