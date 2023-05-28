5d3b64e4833a62de02f64234d1a21f7c6fccdfcf
konevyar <konev.yar@outlook.com>
2023-05-28 14:11:52 +0300

    add custom user model naming

--- project/users/models.py
+++ project/users/models.py
@@ -32,6 +32,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
         return self.email
 
